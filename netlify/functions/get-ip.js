d71694dfed8d94e7b8f883e306669da0af8ef077
Pietro Trevisan <pietrotrevisan@MacBook-Air-di-Pietro.local>
2026-05-06 21:20:04 +0200

    password mostra toggle

---
+++ netlify/functions/get-ip.js
@@ -0,0 +1,16 @@
+exports.handler = async (event) => {
+    const ip =
+        event.headers['x-forwarded-for']?.split(',')[0].trim() ||
+        event.headers['client-ip'] ||
+        event.headers['x-real-ip'] ||
+        null;
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({ ip }),
+    };
+};
